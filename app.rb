cbddb101e181f20ad7778de76f81f7d71f8b25c6
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-07 10:42:43 +0000

    Kill the global variable

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require './lib/attack'
 class Battle < Sinatra::Base
   enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
@@ -13,18 +17,17 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1])
     player_2 = Player.new(params[:player_2])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   post '/attack' do
-    Attack.run($game.opponent_of($game.current_turn))
-    if $game.game_over?
+    Attack.run(@game.opponent_of(@game.current_turn))
+    if @game.game_over?
       redirect '/game-over'
     else
       redirect '/attack'
@@ -32,17 +35,15 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @game = $game
     erb :attack
   end
 
   post '/switch-turns' do
-    $game.switch_turns
+    @game.switch_turns
     redirect '/play'
   end
 
   get '/game-over' do
-    @game = $game
     erb :game_over
   end
 
